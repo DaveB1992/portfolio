3c9830ff9ee4fb9beaa39b8f7fec1541e1d0b0c0
Dave Bratby <dbratby1992@gmail.com>
2023-02-20 18:38:29 +0000

    added a smooth scroll feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const btn = document.querySelector("#button");
+const btn = document.querySelector("#button"); // links the HTML elements to the JS 
 const textBox = document.querySelector("#textBox");
 
 btn.addEventListener("click", () => {
     textBox.value = ""
 })
+
